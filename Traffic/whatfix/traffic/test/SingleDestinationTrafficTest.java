f1bf8b9f0502594903ec0458d393a21d47fa9d5b
santhoshscse <santhosh.s.cse@gmail.com>
2017-08-03 15:57:19 +0530

    usecase added

--- Traffic/whatfix/traffic/test/SingleDestinationTrafficTest.java
+++ Traffic/whatfix/traffic/test/SingleDestinationTrafficTest.java
@@ -16,6 +16,11 @@ public class SingleDestinationTrafficTest {
 		case1();
 		System.out.println("===============");
 		case2();
+		System.out.println("===============");
+		case3();
+		System.out.println("===============");
+		case4();
+
 	}
 
 	private static void case1() {
@@ -24,18 +29,18 @@ public class SingleDestinationTrafficTest {
 		HashMap<String, Orbit> orbitMap = InputUtil.getOrbitMap();
 		List<Orbit> orbitList = new ArrayList<>();
 		Orbit orbit1 = orbitMap.get("Orbit1");
-		orbit1.setTraffic(10);
+		orbit1.setTraffic(12);
 		orbitList.add(orbit1);
 
 		Orbit orbit2 = orbitMap.get("Orbit2");
-		orbit2.setTraffic(12);
+		orbit2.setTraffic(10);
 		orbitList.add(orbit2);
 
 		List<Vehicle> vehicleList = InputUtil.getVehicleList();
 
 		Route route = SingleDestinationTrafficSolution.getShortestRoute(orbitList, vehicleList, weather);
 
-		System.out.println("Case1 result :: " + route.getVehicle().getName() + " " + route.getOrbit().getName());
+		System.out.println("Case3 result :: " + route.getVehicle().getName() + " " + route.getOrbit().getName());
 	}
 
 	private static void case2() {
@@ -56,5 +61,53 @@ public class SingleDestinationTrafficTest {
 		Route route = SingleDestinationTrafficSolution.getShortestRoute(orbitList, vehicleList, weather);
 
 		System.out.println("Case2 result :: " + route.getVehicle().getName() + " " + route.getOrbit().getName());
+	}
+
+	private static void case3() {
+		Weather weather = InputUtil.getSunnyWeather();
+
+		HashMap<String, Orbit> orbitMap = InputUtil.getOrbitMap();
+		List<Orbit> orbitList = new ArrayList<>();
+		Orbit orbit1 = orbitMap.get("Orbit1");
+		orbit1.setTraffic(10);
+		orbitList.add(orbit1);
+
+		Orbit orbit2 = orbitMap.get("Orbit2");
+		orbit2.setTraffic(12);
+		orbitList.add(orbit2);
+
+		List<Vehicle> vehicleList = InputUtil.getVehicleList();
+
+		Route route = SingleDestinationTrafficSolution.getShortestRoute(orbitList, vehicleList, weather);
+
+		System.out.println("Case3 result :: " + route.getVehicle().getName() + " " + route.getOrbit().getName());
+	}
+
+	private static void case4() {
+		Weather weather = InputUtil.getSunnyWeather();
+
+		HashMap<String, Orbit> orbitMap = InputUtil.getOrbitMap();
+		List<Orbit> orbitList = new ArrayList<>();
+		Orbit orbit1 = orbitMap.get("Orbit1");
+		orbit1.setTraffic(10);
+		orbitList.add(orbit1);
+
+		Orbit orbit2 = orbitMap.get("Orbit2");
+		orbit2.setTraffic(12);
+		orbitList.add(orbit2);
+
+		Orbit orbit3 = orbitMap.get("Orbit3");
+		orbit3.setTraffic(15);
+		orbitList.add(orbit3);
+
+		Orbit orbit4 = orbitMap.get("Orbit4");
+		orbit4.setTraffic(12);
+		orbitList.add(orbit4);
+
+		List<Vehicle> vehicleList = InputUtil.getVehicleList();
+
+		Route route = SingleDestinationTrafficSolution.getShortestRoute(orbitList, vehicleList, weather);
+
+		System.out.println("Case4 result :: " + route.getVehicle().getName() + " " + route.getOrbit().getName());
 	}
 }
